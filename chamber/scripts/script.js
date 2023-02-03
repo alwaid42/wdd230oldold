f7a55b181f7457a5b9944eee14fda1594d19d176
alwaid42 <ronaldomschmidt@gmail.com>
2023-02-03 02:31:23 -0300

    W04

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,6 +1,13 @@
-let date = new Date();
-let currentYear = date.getFullYear();
+const date = new Date();
+const currentYear = date.getFullYear();
 document.querySelector('#current-year').textContent = currentYear;
 
-let lastModified = document.lastModified;
+const lastModified = document.lastModified;
 document.getElementById('last-modified').textContent = lastModified;
+
+const datefield = document.querySelector('.date');
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
