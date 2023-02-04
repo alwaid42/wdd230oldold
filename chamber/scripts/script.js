83f514de364c0f704124c9f01d7a3d9cebf34cb5
alwaid42 <ronaldomschmidt@gmail.com>
2023-02-04 01:15:55 -0300

    W05

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -5,9 +5,22 @@ document.querySelector('#current-year').textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('last-modified').textContent = lastModified;
 
-const datefield = document.querySelector('.date');
-
-
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const fulldate = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(date);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+const weekday = date.getDay();
+if (weekday == 1 || weekday == 2){
+    document.getElementById("banner").style.display = "block";
+}
