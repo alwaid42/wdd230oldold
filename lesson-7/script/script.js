5167b5cab2aa960dee0f8012dac2ca32c3e577d2
alwaid42 <ronaldomschmidt@gmail.com>
2023-02-17 02:01:23 -0300

    Lazy Loading

--- lesson-7/script/script.js
+++ lesson-7/script/script.js
@@ -1,28 +1,29 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
-    threshold: 0,
+    threshold: 1, 
     rootMargin: "0px 0px 50px 0px"
 };
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute("data-src"));
-    image.onLoad = () => {
-        image.removeAttribute("data-src");
+
+const loadImages = (image) => { 
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
     };
 };
 
-if('IntersectionObserver' in window) {
-    const observer  = new IntersectionObserver((items, observer) => {
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items) => {
         items.forEach((item) => {
             if (item.isIntersecting) {
                 loadImages(item.target);
-                observer.unobserve(item.target);
+                imgObserver.unobserve(item.target);
             }
         });
-    });
+    }, imgOptions);
     imagesToLoad.forEach((img) => {
-        observer.observe(img);
+        imgObserver.observe(img);
     });
 } else {
     imagesToLoad.forEach((img) => {
